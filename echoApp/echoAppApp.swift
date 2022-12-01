5803f6825580776298db562d0227bf51f2113b04
FelixAnthonisen <felix.anthonisen@gmail.com>
2022-12-01 14:47:01 +0100

    Initial Commit

---
+++ echoApp/echoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  echoAppApp.swift
+//  echoApp
+//
+//  Created by Felix Anthonisen on 01/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct echoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

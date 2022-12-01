fcc8fc70882cfa5ba0ef36911215ccae0840e408
FelixAnthonisen <felix.anthonisen@gmail.com>
2022-12-01 16:50:32 +0100

    yes

--- echoApp/Views/Profile.swift
+++ echoApp/Views/Profile.swift
@@ -10,39 +10,56 @@ import SwiftUI
 struct Profile: View {
     @EnvironmentObject var session: SessionManager
     var body: some View {
-        VStack {
-            ZStack {
-                Image(systemName: "person.crop.circle")
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .frame(height: 100)
-                Image(systemName: "circle.fill")
-                    .imageScale(.large)
-                    .offset(x: 25, y: 40)
-                    .foregroundColor(.white)
-                Image(systemName: "camera.circle")
-                    .imageScale(.large)
-                    .offset(x: 25, y: 40)
-                    .foregroundColor(.black)
-            }
-            
-            Text("Torger Bocianowski")
-                .font(Font.system(size: 30, weight: .bold))
-                .padding()
+        HStack{
             Spacer()
-            Button(action: {
-                withAnimation {
-                    session.logout()
+            VStack {
+                ZStack {
+                    Image(systemName: "person.crop.circle")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(height: 100)
+                        .foregroundColor(.white)
+                    Image(systemName: "circle.fill")
+                        .imageScale(.large)
+                        .offset(x: 25, y: 40)
+                        .foregroundColor(.white)
+                    Image(systemName: "camera.circle")
+                        .imageScale(.large)
+                        .offset(x: 25, y: 40)
+                        .foregroundColor(.black)
                 }
-            }) {
-                Text("Logg ut").font(Font.system(size: 22))
+                
+                Text("Torger Bocianowski")
+                    .font(Font.system(size: 30, weight: .bold))
+                    .foregroundColor(.white)
+                    .padding()
+                Spacer()
+                Button(action: {
+                    withAnimation {
+                        session.logout()
+                    }
+                }){
+                    Text("Logg ut")
+                        .foregroundColor(.white)
+                        .font(Font.system(size: 22))
+                }
+                .padding([.horizontal], 20)
+                .padding([.vertical], 10)
+                .fontWeight(.bold)
+                .background(Color.blue)
+
+                .foregroundColor(.white)
+                .cornerRadius(20)
             }
-        }.padding()
+            .padding(.top)
+            Spacer()
+        }
+       
     }
 }
 
 struct Profile_Previews: PreviewProvider {
     static var previews: some View {
-        Profile()
+        Profile().background(.black)
     }
 }
